a558e59c68e76391c1a6597a63d0bc5388bdcea4
udhayadharani <udhayarajendran7090@gmail.com>
2022-10-01 13:06:44 +0530

    first commit

---
+++ Student1.java
@@ -0,0 +1,48 @@
+package com.collage;
+
+public class Student1 {
+    private String name;
+    private String age;
+    private String emailId;
+    private String qualification;
+
+
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public String getAge() {
+
+        return age;
+    }
+
+    public void setAge(String age) {
+
+        this.age = age;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public  void setEmailId(String emailId) {
+
+        this.emailId = emailId;
+    }
+
+    public String getQualification() {
+
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+}
